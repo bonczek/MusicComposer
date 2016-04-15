46c2d8d9fe3645599a8b4525500d95e4cef58dfb
Adam Bąk <adam-bak@o2.pl>
2016-04-15 12:13:56 +0200

    reward for statistical Towsey fitness function

--- src/main/java/genetic/fitness/towsey/TowseyMusicalFitness.java
+++ src/main/java/genetic/fitness/towsey/TowseyMusicalFitness.java
@@ -8,28 +8,32 @@ import music.analysis.towsey.statistics.RhythmVarietyStatistic;
 import music.analysis.towsey.statistics.RhythmicRangeStatistic;
 import music.analysis.towsey.statistics.TowseyStatistic;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TowseyMusicalFitness extends MusicalFitnessFunction {
 
-    private List<TowseyStatistic> statistics = new ArrayList<>();
+    private Map<TowseyStatistic, Double> statisticsExpectedMap = new HashMap<>();
 
     public TowseyMusicalFitness() {
-        statistics.add(new PitchVarietyStatistic());
-        statistics.add(new DissonanceStatistic());
-        statistics.add(new RhythmVarietyStatistic(16));
-        statistics.add(new RhythmicRangeStatistic(16));
+        statisticsExpectedMap.put(new PitchVarietyStatistic(), 0.27);
+        statisticsExpectedMap.put(new DissonanceStatistic(), 0.01);
+        statisticsExpectedMap.put(new RhythmVarietyStatistic(16), 0.24);
+        statisticsExpectedMap.put(new RhythmicRangeStatistic(16), 0.32);
     }
 
     @Override
     protected Integer rateMelody(List<Note> noteList) {
         for (Note note : noteList) {
-            statistics.stream().forEach(statistic -> statistic.processNote(note));
+            statisticsExpectedMap.keySet().stream().forEach(statistic -> statistic.processNote(note));
         }
 
-        statistics.stream().mapToDouble(TowseyStatistic::getResult).sum();
-        //@todo how to rate statistical approach?
-        return 0;
+        List<Double> statisticDifferences = statisticsExpectedMap.entrySet().stream().map(entry -> Math.abs(entry
+                .getKey().getResult() - entry.getValue())).collect(Collectors.toList());
+        List<Double> rewards = statisticDifferences.stream().map(value -> 100 * Math.cos(Math.PI * value)).collect(Collectors.toList());
+        Double rewardSum = rewards.stream().mapToDouble(d -> d).sum();
+        return rewardSum.intValue();
     }
 }
--- src/test/java/genetic/fitness/towsey/TowseyMusicalFitnessTest.java
+++ src/test/java/genetic/fitness/towsey/TowseyMusicalFitnessTest.java
@@ -1,8 +1,12 @@
 package genetic.fitness.towsey;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
+import genetic.fitness.FitnessFunction;
 import genetic.representation.Chromosome;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TowseyMusicalFitnessTest {
 
@@ -10,11 +14,10 @@ public class TowseyMusicalFitnessTest {
     public void testRateChromosome() throws Exception {
         Integer[] geneValues = {23, -2, -2, 31, -1, -2, 79, 55, -1, -2, -2, -2, -2, 120, 63, 30, 22, 79, 14, 45, 46,
                 75, 37, 62, 121};
-        Chromosome testChromosome = Chromosome.createWithIntegerValues(Arrays.asList(geneValues));
-        TowseyMusicalFitness calculator = new TowseyMusicalFitness();
+        Chromosome[] testChromosome = {Chromosome.createWithIntegerValues(Arrays.asList(geneValues))};
+        FitnessFunction calculator = new TowseyMusicalFitness();
 
-        //int a = calculator.rateChromosome(testChromosome);
-        //int b = a;
-
+        calculator.calculateFitness(Arrays.asList(testChromosome));
+        assertThat(testChromosome[0].getFitness(), is(70));
     }
 }
