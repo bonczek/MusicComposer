5a5c04325715d66f75a07717cbbc37847e524f51
Adam Bąk <adam-bak@o2.pl>
2016-06-29 20:44:17 +0200

    new rules preparation

--- src/main/java/genetic/fitness/rules/RuleFitnessFunction.java
+++ src/main/java/genetic/fitness/rules/RuleFitnessFunction.java
@@ -15,10 +15,14 @@ public class RuleFitnessFunction extends MusicalFitnessFunction {
     private List<MusicalRule> musicalRules = new ArrayList<>();
 
     public RuleFitnessFunction() {
-        musicalRules.add(new MelodicIntervalRule(i -> !i.moreThanOctave() && i.perfectConsonance()));
-        musicalRules.add(new MelodicIntervalRule(i -> !i.moreThanOctave() && i.imperfectConsonance()));
-        musicalRules.add(new MelodicIntervalRule(i -> !i.moreThanOctave() && i.dissonance()));
-        musicalRules.add(new MelodicIntervalRule(Interval::moreThanOctave));
+        musicalRules.add(new MelodicIntervalRule(Interval::perfectConsonance));
+        musicalRules.add(new MelodicIntervalRule(Interval::imperfectConsonance));
+        musicalRules.add(new MelodicIntervalRule(Interval::dissonance));
+        musicalRules.add(new MelodicIntervalRule(i -> !i.moreThanOctave()));
+//        musicalRules.add(new NoteRule(n -> n.getRhythmValue() >= Durations.QUARTER_NOTE));
+//        musicalRules.add(new NoteRule(n -> n instanceof Rest));
+//        musicalRules.add(new NoteRule(n -> n instanceof Sound));
+
     }
 
     @Override
@@ -28,15 +32,21 @@ public class RuleFitnessFunction extends MusicalFitnessFunction {
             musicalRules.stream().forEach(r -> r.processNote(n));
         }
         RuleFitness fitness = new RuleFitness();
-        Rule perfect = new Rule("PERFECT_CONSONANCE", 80, musicalRules.get(0).ruleCount());
-        Rule imperfect = new Rule("IMPERFECT_CONSONANCE", 60, musicalRules.get(1).ruleCount());
+        Rule perfect = new Rule("PERFECT_CONSONANCE", 50, musicalRules.get(0).ruleCount());
+        Rule imperfect = new Rule("IMPERFECT_CONSONANCE", 50, musicalRules.get(1).ruleCount());
         Rule dissonance = new Rule("DISSONANCE", 30, musicalRules.get(2).ruleCount());
-        Rule moreThanOctave = new Rule("MORE_THAN_OCTAVE", 0, musicalRules.get(3).ruleCount());
+        Rule lessThanOctave = new Rule("LESS_THAN_OCTAVE", 60, musicalRules.get(3).ruleCount());
+        Rule longNotes = new Rule("LONG_NOTES", 50, musicalRules.get(4).ruleCount());
+        Rule restNotes = new Rule("REST_NOTES", 50, musicalRules.get(5).ruleCount());
+        Rule soundNotes = new Rule("SOUND_NOTES", 50, musicalRules.get(6).ruleCount());
 
         fitness.addRuleReward(perfect);
         fitness.addRuleReward(imperfect);
         fitness.addRuleReward(dissonance);
-        fitness.addRuleReward(moreThanOctave);
+        fitness.addRuleReward(lessThanOctave);
+        fitness.addRuleReward(longNotes);
+        fitness.addRuleReward(restNotes);
+        fitness.addRuleReward(soundNotes);
 
         return fitness;
     }
