1988dcaab07d3f63a892cd5569f7f176bbc8351c
Adam Bąk <adam-bak@o2.pl>
2016-07-27 11:03:40 +0200

    undo changes in semitones difference

--- src/main/java/music/analysis/feature/processor/statistics/intervals/SemitonesDifferenceStatistic.java
+++ src/main/java/music/analysis/feature/processor/statistics/intervals/SemitonesDifferenceStatistic.java
@@ -1,15 +1,14 @@
 package music.analysis.feature.processor.statistics.intervals;
 
 import music.notes.pitch.Interval;
+import music.notes.pitch.Pitch;
 
 //@todo not used statistic
 public class SemitonesDifferenceStatistic extends IntervalStatistic {
 
-    private static final int MAX_ACCEPTABLE_INTERVALS_RANGE = 24;
-
     @Override
     protected void processInterval(Interval interval) {
         numerator += Math.abs(interval.getSemitonesDifference());
-        denominator += MAX_ACCEPTABLE_INTERVALS_RANGE;
+        denominator += Pitch.MAX_MIDI_VALUE;
     }
 }
